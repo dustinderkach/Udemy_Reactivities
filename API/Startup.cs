bfdb1791398f48eedadab70bba14ddf272b17057
Dustin Derkach <Dustin.Derkach@potashcorp.com>
2020-07-15 22:52:26 -0600

    Just Installed react app client-app

--- API/Startup.cs
+++ API/Startup.cs
@@ -35,7 +35,8 @@ namespace API
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. 
+        //Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
