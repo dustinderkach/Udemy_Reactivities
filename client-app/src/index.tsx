640334fe35f02da14ef9ea455a2c7197343c8866
dustinderkach <dustinderkach2@gmail.com>
2021-01-23 11:51:07 -0600

    remove React.StrictMode (findDOMNode error)

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -4,10 +4,14 @@ import './app/layout/styles.css';
 import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
 
+//Removed the <React.StrictMode> <App /></React.StrictMode>
+//tags to prevent the Warning: findDOMNode is deprecated in StrictMode
+// "Some 3rd party libraries will fail the strict mode as they still need to be updated for React 17, which does not currently have a  release date."
+
 ReactDOM.render(
-  <React.StrictMode>
+
     <App />
-  </React.StrictMode>,
+,
   document.getElementById('root')
 );
 
